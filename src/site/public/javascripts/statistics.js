3c08fe660219a9d8ba5963b6d973b7865b4b0cbb
Пользователь <>
2018-01-07 13:17:13 +0800

    ui changes

--- src/site/public/javascripts/statistics.js
+++ src/site/public/javascripts/statistics.js
@@ -1,5 +1,46 @@
 $(document).ready(function() {
 
+
+    var options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: {
+            duration: 0
+        },
+        tooltips: {
+            callbacks: {
+                title: function(tooltipItem, data) {
+                    return moment(tooltipItem[0].xLabel).format("DD-MM-YYYY HH:mm");
+                }
+            }
+        },
+        elements: {
+            point: { 
+                radius: 2 ,
+                hitRadius: 3, 
+                hoverRadius: 3
+            }
+        },
+        scales: {
+            xAxes: [{
+                type: "time",
+                time: {
+                    displayFormats: {
+                        'millisecond': 'HH:mm:ss',
+                        'second': 'HH:mm:ss',
+                        'minute': 'HH:mm',
+                        'hour': 'HH:mm',
+                        'day': 'MMM D',
+                        'week': 'll',
+                        'month': 'MMM YYYY',
+                        'quarter': '[Q]Q - YYYY',
+                        'year': 'YYYY',
+                     }
+                }
+            }]
+        }
+    };
+
     $.getJSON("/api/statistics/" + videoId, function(data) 
     {
         var ctxLike = document.getElementById('statisticsLikeChart').getContext('2d');
@@ -23,25 +64,7 @@ $(document).ready(function() {
                     data: data.map((d) => {return {x: new Date (d.updatedAt), y:d.dislikeCount } })
                 }]
             },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                animation: {
-                    duration: 0
-                },
-                elements: {
-                    point: { 
-                        radius: 2 ,
-                        hitRadius: 3, 
-                        hoverRadius: 3
-                    }
-                },
-                scales: {
-					xAxes: [{
-                        type: "time"
-                    }]
-                }
-            }
+            options: options
         });
 
 
@@ -58,25 +81,7 @@ $(document).ready(function() {
                     data: data.map((d) => {return {x: new Date (d.updatedAt), y:d.viewCount } })
                 }]
             },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                elements: {
-                    point: { 
-                        radius: 2 ,
-                        hitRadius: 3, 
-                        hoverRadius: 3
-                    }
-                },
-                animation: {
-                    duration: 0
-                },
-                scales: {
-					xAxes: [{
-                        type: "time"
-                    }]
-                }
-            }
+            options: options
         });
 
     });
