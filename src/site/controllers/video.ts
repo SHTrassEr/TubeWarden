a22fbca69406f99e61821bc0d49ceae6f500341d
Пользователь <>
2018-01-26 14:59:25 +0800

    add video tags to page

--- src/site/controllers/video.ts
+++ src/site/controllers/video.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import Channel from "../../models/db/channel";
+import Tag from "../../models/db/tag";
 import Video from "../../models/db/video";
 
 export async function getVideo(req: Request, res: Response) {
     const video = await Video.findOne({
-        include: [Channel],
+        include: [Channel, Tag],
         where: {
             videoId: req.params.videoId,
         },
